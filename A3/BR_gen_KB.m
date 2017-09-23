7e8e5c1cfcdd7496a82fb61cd758e992ca6fa428
haochenz <haochenz@lab3b-11.users.coe.utah.edu>
2017-09-23 14:49:43 -0600

    Half way through KB

--- A3/BR_gen_KB.m
+++ A3/BR_gen_KB.m
@@ -39,9 +39,131 @@ function [KB,KBi,vars] = BR_gen_KB
 vars = 1:80;
 
 % The 4 cells around a P have B
+for i = 1:16
+    index = length(KB) + 1;
+    cur_i = index;
+    
+    % There must exist a -Bxy ^ ... no matter what.
+    KB(index).clauses(end+1) = "-B" + (mod(i, 4)) + (floor(i/4) + 1);
+    KBi(index).clauses(end+1) = -(16 + i);
+    
+    % Cells not on the left edge
+    if mod(i, 4) == 1
+        cur_i = index + 1;
+        KB(index).clauses(end+1) =  "P" + (mod(i, 4) + 1) + (floor(i/4) + 1);
+        KB(cur_i).clauses(end+1) = "-P" + (mod(i, 4) + 1) + (floor(i/4) + 1);
+        KB(cur_i).clauses(end+1) =  "B" + (mod(i, 4)) + (floor(i/4) + 1);
+        
+        KBi(index).clauses(end+1) = i + 1;
+        KBi(cur_i).clauses(end+1) = -(i + 1);
+        KBi(cur_i).clauses(end+1) =  16 + i;
+    end
+    
+    % Cells not on the right edge
+    if mod(i, 4) ~= 0
+        cur_i = index + 1;
+        KB(index).clauses(end+1) =  "P" + (mod(i, 4) - 1) + (floor(i/4) + 1);
+        KB(cur_i).clauses(end+1) = "-P" + (mod(i, 4) - 1) + (floor(i/4) + 1);
+        KB(cur_i).clauses(end+1) =  "B" + (mod(i, 4)) + (floor(i/4) + 1);
+        
+        KBi(index).clauses(end+1) = i - 1;
+        KBi(cur_i).clauses(end+1) = -(i - 1);
+        KBi(cur_i).clauses(end+1) =  16 + i;
+    end
+    
+    % Cells not on the bottom edge
+    if i > 4
+        cur_i = index + 1;
+        KB(index).clauses(end+1) =  "P" + (mod(i, 4)) + (floor(i/4));
+        KB(cur_i).clauses(end+1) = "-P" + (mod(i, 4)) + (floor(i/4));
+        KB(cur_i).clauses(end+1) =  "B" + (mod(i, 4)) + (floor(i/4) + 1);
+        
+        KBi(index).clauses(end+1) = i - 4;
+        KBi(cur_i).clauses(end+1) = -(i - 4);
+        KBi(cur_i).clauses(end+1) =  16 + i;
+    end
+    
+    % Cells not on the top edge
+    if i < 13
+        cur_i = index + 1;
+        KB(index).clauses(end+1) =  "P" + (mod(i, 4)) + (floor(i/4) + 2);
+        KB(cur_i).clauses(end+1) = "-P" + (mod(i, 4)) + (floor(i/4) + 2);
+        KB(cur_i).clauses(end+1) =  "B" + (mod(i, 4)) + (floor(i/4) + 1);
+        
+        KBi(index).clauses(end+1) = i + 4;
+        KBi(cur_i).clauses(end+1) = -(i + 4);
+        KBi(cur_i).clauses(end+1) =  16 + i;
+    end
+end
 
 % The 4 cells around a Wampus have S
+for i = 1:16
+    index = length(KB) + 1;
+    cur_i = index;
+    
+    % There must exist a -Sxy ^ ... no matter what.
+    KB(index).clauses(end+1) = "-S" + (mod(i, 4)) + (floor(i/4) + 1);
+    KBi(index).clauses(end+1) = -(16 + i + 32);
+    
+    % Cells not on the left edge
+    if mod(i, 4) == 1
+        cur_i = index + 1;
+        KB(index).clauses(end+1) =  "W" + (mod(i, 4) + 1) + (floor(i/4) + 1);
+        KB(cur_i).clauses(end+1) = "-W" + (mod(i, 4) + 1) + (floor(i/4) + 1);
+        KB(cur_i).clauses(end+1) =  "S" + (mod(i, 4)) + (floor(i/4) + 1);
+        
+        KBi(index).clauses(end+1) = i + 1 + 64;
+        KBi(cur_i).clauses(end+1) = -(i + 1 + 64);
+        KBi(cur_i).clauses(end+1) =  16 + i + 32;
+    end
+    
+    % Cells not on the right edge
+    if mod(i, 4) ~= 0
+        cur_i = index + 1;
+        KB(index).clauses(end+1) =  "W" + (mod(i, 4) - 1) + (floor(i/4) + 1);
+        KB(cur_i).clauses(end+1) = "-W" + (mod(i, 4) - 1) + (floor(i/4) + 1);
+        KB(cur_i).clauses(end+1) =  "S" + (mod(i, 4)) + (floor(i/4) + 1);
+        
+        KBi(index).clauses(end+1) = i - 1 + 64;
+        KBi(cur_i).clauses(end+1) = -(i - 1 + 64);
+        KBi(cur_i).clauses(end+1) =  16 + i + 32;
+    end
+    
+    % Cells not on the bottom edge
+    if i > 4
+        cur_i = index + 1;
+        KB(index).clauses(end+1) =  "w" + (mod(i, 4)) + (floor(i/4));
+        KB(cur_i).clauses(end+1) = "-W" + (mod(i, 4)) + (floor(i/4));
+        KB(cur_i).clauses(end+1) =  "S" + (mod(i, 4)) + (floor(i/4) + 1);
+        
+        KBi(index).clauses(end+1) = i - 4 + 64;
+        KBi(cur_i).clauses(end+1) = -(i - 4 + 64);
+        KBi(cur_i).clauses(end+1) =  16 + i + 32;
+    end
+    
+    % Cells not on the top edge
+    if i < 13
+        cur_i = index + 1;
+        KB(index).clauses(end+1) =  "W" + (mod(i, 4)) + (floor(i/4) + 2);
+        KB(cur_i).clauses(end+1) = "-W" + (mod(i, 4)) + (floor(i/4) + 2);
+        KB(cur_i).clauses(end+1) =  "S" + (mod(i, 4)) + (floor(i/4) + 1);
+        
+        KBi(index).clauses(end+1) = i + 4 + 64;
+        KBi(cur_i).clauses(end+1) = -(i + 4 + 64);
+        KBi(cur_i).clauses(end+1) =  16 + i + 32;
+    end
+end
 
-% There is at most 1 Wampus
+% There is at most 1 Wampusfor i = 1:16
+for i = 1:16
+    KB(end + 1).clauses(end+1) = "-W" + (mod(i, 4)) + (floor(i/4) + 2);
+    KB(end).clauses(end+1) =  "-W" + (mod(i, 4)) + (floor(i/4) + 1);
+
+    KBi(end + 1).clauses(end+1) = -(i + 64);
+    KBi(end).clauses(end+1) = -(i + 64);
+end
 
 % There is exactly 1 gold
+for i = 1:16
+    
+end
