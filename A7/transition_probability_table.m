63bf25766895f77963e525d2a0551a6b91aed839
jawtyngw <jawtyngw@lab3b-19.users.coe.utah.edu>
2017-11-21 15:57:38 -0700

    header

--- A7/transition_probability_table.m
+++ A7/transition_probability_table.m
@@ -1,14 +1,18 @@
-function cell_p = transition_probability_table(S, action)
+function P = transition_probability_table()
 % transition_probability_table - return destination cell and their...
 %        probabilities
-% On input:
-%       S (1*2 vector): state (x,y)
-%       A (int): actions (1 to 4)
 % On output:
-%       P (nx3 double matrix): transition model
-%         P(:,1): x index of cell
-%         P(:,2): y index of cell
-%         P(:,3): Probability
+%       P (nxk struct array): transition model
+%           (s,a).probs (a vector with n transition probabilities
+%           (from s to s_prime, given action a)
+%
+%       Layout:                1
+%          13 14 15 16         ˆ
+%           9 10 11 12         |
+%           5  6  7  8     2 <- -> 4
+%           1  2  3  4         |
+%                              V
+%                              3
 % Call:
 %       cell = [1,1];
 %       cell_p = transition_probability_table(cell, 1);
@@ -17,7 +21,7 @@ function cell_p = transition_probability_table(S, action)
 %       UU
 %       Fall 2017
 %
-cell_p = [];
+P = [];
 x = S(1);
 y = S(2);
 neis = BR_Wumpus_neighbors(x,y);
@@ -44,94 +48,94 @@ switch action
     case 1
         % UP
         if ismember(1,possible_dirs)
-            cell_p = [cell_p; x, y+1, 0.8];
+            P = [P; x, y+1, 0.8];
         else
-            cell_p = [cell_p; x, y, 0.8];
+            P = [P; x, y, 0.8];
         end
         % Left
         if ismember(2,possible_dirs)
-            cell_p = [cell_p; x-1, y, 0.1];
+            P = [P; x-1, y, 0.1];
         else
-            cell_p = [cell_p; x, y, 0.1];
+            P = [P; x, y, 0.1];
         end
         % Right
         if ismember(3,possible_dirs)
-            cell_p = [cell_p; x+1, y, 0.1];
+            P = [P; x+1, y, 0.1];
         else
-            cell_p = [cell_p; x, y, 0.1];
+            P = [P; x, y, 0.1];
         end
         
     % Action: Left
     case 2
         % Left
         if ismember(2,possible_dirs)
-            cell_p = [cell_p; x-1, y, 0.8];
+            P = [P; x-1, y, 0.8];
         else
-            cell_p = [cell_p; x, y, 0.8];
+            P = [P; x, y, 0.8];
         end
         % Up
         if ismember(1,possible_dirs)
-            cell_p = [cell_p; x, y+1, 0.1];
+            P = [P; x, y+1, 0.1];
         else
-            cell_p = [cell_p; x, y, 0.1];
+            P = [P; x, y, 0.1];
         end
         % Down
         if ismember(0,possible_dirs)
-            cell_p = [cell_p; x, y-1, 0.1];
+            P = [P; x, y-1, 0.1];
         else
-            cell_p = [cell_p; x, y, 0.1];
+            P = [P; x, y, 0.1];
         end
         
     % Action: Down
     case 3
         % Down
         if ismember(0,possible_dirs)
-            cell_p = [cell_p; x, y-1, 0.8];
+            P = [P; x, y-1, 0.8];
         else
-            cell_p = [cell_p; x, y, 0.8];
+            P = [P; x, y, 0.8];
         end
         % Left
         if ismember(2,possible_dirs)
-            cell_p = [cell_p; x-1, y, 0.1];
+            P = [P; x-1, y, 0.1];
         else
-            cell_p = [cell_p; x, y, 0.1];
+            P = [P; x, y, 0.1];
         end
         % Right
         if ismember(3,possible_dirs)
-            cell_p = [cell_p; x+1, y, 0.1];
+            P = [P; x+1, y, 0.1];
         else
-            cell_p = [cell_p; x, y, 0.1];
+            P = [P; x, y, 0.1];
         end
     % Action: Right
     case 4
         % Right
         if ismember(4,possible_dirs)
-            cell_p = [cell_p; x+1, y, 0.8];
+            P = [P; x+1, y, 0.8];
         else
-            cell_p = [cell_p; x, y, 0.8];
+            P = [P; x, y, 0.8];
         end
         % UP
         if ismember(1,possible_dirs)
-            cell_p = [cell_p; x, y+1, 0.1];
+            P = [P; x, y+1, 0.1];
         else
-            cell_p = [cell_p; x, y, 0.1];
+            P = [P; x, y, 0.1];
         end
         % Down
         if ismember(0,possible_dirs)
-            cell_p = [cell_p; x, y-1, 0.1];
+            P = [P; x, y-1, 0.1];
         else
-            cell_p = [cell_p; x, y, 0.1];
+            P = [P; x, y, 0.1];
         end
 end
 
 % Combine deplicated cell
 % do it backwards
-len_cell_p = length(cell_p(:,1));
+len_cell_p = length(P(:,1));
 for i = 0:len_cell_p - 2
-    if (cell_p(len_cell_p - i,1) == cell_p(1,1) &&...
-            cell_p(len_cell_p - i,2) == cell_p(1,2))
-        cell_p(1,3) = cell_p(len_cell_p - i,3) + cell_p(1,3);
-        cell_p(len_cell_p - i,:) = [];
+    if (P(len_cell_p - i,1) == P(1,1) &&...
+            P(len_cell_p - i,2) == P(1,2))
+        P(1,3) = P(len_cell_p - i,3) + P(1,3);
+        P(len_cell_p - i,:) = [];
     end
 end
 
